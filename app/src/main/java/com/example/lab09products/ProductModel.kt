7befa7d3ef0282d47474746aaa0c8ddaa79e2dad
Josue123212 <choquepumajosue@gmail.com>
2025-10-24 22:17:40 -0500

    "Creacion de ProductApiService.kt"

--- app/src/main/java/com/example/lab09products/ProductModel.kt
+++ app/src/main/java/com/example/lab09products/ProductModel.kt
@@ -1,2 +1,9 @@
 package com.example.lab09products
 
+data class ProductModel(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("price") val price: Double,
+    @SerializedName("thumbnail") val thumbnail: String
+)
