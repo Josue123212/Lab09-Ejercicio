7bbbaf6034190294a9656aa21d8b8422d604425b
Josue123212 <choquepumajosue@gmail.com>
2025-10-24 22:19:16 -0500

    "Creacion de Screens.kt"

--- app/src/main/java/com/example/lab09products/MainActivity.kt
+++ app/src/main/java/com/example/lab09products/MainActivity.kt
@@ -13,35 +13,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab09products.ui.theme.Lab09ProductsTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Lab09ProductsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainApp() {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://dummyjson.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val servicio = retrofit.create(ProductApiService::class.java)
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = { TopBar() },
+        content = { padding -> Content(padding, navController, servicio) }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab09ProductsTheme {
-        Greeting("Android")
-    }
 }
--- app/src/main/java/com/example/lab09products/ProductApiService.kt
+++ app/src/main/java/com/example/lab09products/ProductApiService.kt
@@ -1,2 +1,9 @@
 package com.example.lab09products
 
+interface ProductApiService {
+    @GET("products")
+    suspend fun getProducts(): List<ProductModel>
+
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): ProductModel
+}
